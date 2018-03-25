b602d0c55741e2756711797780876b701fb590ce
ManuelDAW <34402912+ManuelDAW@users.noreply.github.com>
2018-03-25 17:37:56 +0200

    Update jugar.php

--- jugar.php
+++ jugar.php
@@ -10,6 +10,9 @@ $colores = ['Azul', 'Rojo', 'Naranja',
 
 //Inicializo variables generales
 $fomulario = mostrar_formulario($colores);
+$msj="<h3 class='titulo1'>Sección de información varia</h3>";
+$opciones_juego = "Mostrar Clave";
+
 
 //Leemos / generamos la clave
 if (isset($_SESSION['clave']))
@@ -26,11 +29,16 @@ else {
  * La visualizo, también cambiamos el texto
  * que visualizará el botón
  */
+
 if (($_POST['clave']) == "Mostrar Clave") {
-    $msj.="<h3>CLAVE ACTUAL:<br />" . mostrar_clave($clave) . "</h4>";
+    $msj.="<h3 class='titulo'>CLAVE ACTUAL:</h3>";
+    $msj.="<h4>". mostrar_clave($clave) . "</h4>";
     $opciones_juego = "Ocultar Clave";
-} else
+}
+
+if (($_POST['clave']) == "Ocultar Clave") {
     $opciones_juego = "Mostrar Clave";
+}
 
 
 /* AHORA VAMOS A JUGAR* /
@@ -48,10 +56,10 @@ if (isset($_POST['jugar'])) {
     $_SESSION['resultados'][] = $rtdo;
 
     //Pongo informaicón de la jugada actual
-    $num_jugada = sizeof($_SESSION['jugaas']);
+    $num_jugada = sizeof($_SESSION['jugadas']);
     $col = $rtdo['col'];
     $pos = $rtdo['pos'];
-    $msj .= "<h3 class='titulo'>Información de las jugadas </h3>";
+    $msj .= "<h3 class='titulo'>Jugadas realizadas </h3>";
     $msj .= "<h3>Jugada actual $num_jugada </h3>";
     $msj .= "<h3>Resultado : $col Colores y $pos posiciones </h3>";
 
@@ -60,56 +68,51 @@ if (isset($_POST['jugar'])) {
     $msj.=mostrar_resultados();
 
 
+ 
 }
+
+
 ?>
-
-
-
-
-
-
-
-
-
-
 
 
 <!doctype html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>Document</title>
-        <link rel="stylesheet" href="estilo.css" type="text/css"/>
-        <script type="text/javascript">
-            function cambia_color(numero) {
-                var color = document.getElementById("combinacion" + numero).value;
-                var elemento = document.getElementById("combinacion" + numero);
-                elemento.className = color;
-            }
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="estilo.css" type="text/css"/>
+    <script type="text/javascript">
+        function cambia_color(numero) {
+            var color = document.getElementById("combinacion" + numero).value;
+            var elemento = document.getElementById("combinacion" + numero);
+            elemento.className = color;
+        }
 
 
-        </script>
-    </head>
-    <body>
-        <fieldset id="opciones" >
-            <legend>Opciones de juego</legend>
-            <form action="jugar.php" method="POST">
-                <input type="submit" value="<?php echo $opciones_juego
-?>" name="clave" />
-            </form>
+    </script>
+</head>
+<body>
+<fieldset id="opciones" >
+    <legend>Opciones de juego</legend>
+    <form action="jugar.php" method="POST">
+        <input type="submit" value="<?php echo $opciones_juego ?>" name="clave" />
+    </form>
 
-        </fieldset>
-        <fieldset id="informacion">
-            <legend>Información de jugadas</legend>
-            <?php echo $msj ?>
-        </fieldset>>
+</fieldset>
+<fieldset id="informacion">
+    <legend>Información de jugadas</legend>
+    <?php echo $msj ?>
+</fieldset>>
 
-        <fieldset id="jugadas">
-            <legend>Juega</legend>
-            <?php echo $fomulario ?>
-        </fieldset>
+<fieldset id="jugadas">
+    <legend>Juega</legend>
+    <?php echo $fomulario ?>
+</fieldset>
 
-    </body>
+</body>
 
 </body>
 </html>
+
+
+
